f62ae87bef00ac9c987c66aa0ae93bc6f8a42835
Rashika <104192441+Rashiie@users.noreply.github.com>
2023-10-12 20:54:31 +0530

    Create RomanToInteger.java

---
+++ RomanToInteger.java
@@ -0,0 +1,46 @@
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Scanner;
+
+public class RomanToInteger {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Enter a Roman numeral: ");
+        String romanNumeral = scanner.nextLine();
+
+        int result = romanToInt(romanNumeral);
+        System.out.println("Integer value: " + result);
+
+        scanner.close();
+    }
+
+    public static int romanToInt(String s) {
+        Map<Character, Integer> romanToInteger = new HashMap<>();
+        romanToInteger.put('I', 1);
+        romanToInteger.put('V', 5);
+        romanToInteger.put('X', 10);
+        romanToInteger.put('L', 50);
+        romanToInteger.put('C', 100);
+        romanToInteger.put('D', 500);
+        romanToInteger.put('M', 1000);
+
+        int result = 0;
+        int prevValue = 0;
+
+        for (int i = s.length() - 1; i >= 0; i--) {
+            char c = s.charAt(i);
+            int value = romanToInteger.get(c);
+
+            if (value < prevValue) {
+                result -= value;
+            } else {
+                result += value;
+            }
+
+            prevValue = value;
+        }
+
+        return result;
+    }
+}
