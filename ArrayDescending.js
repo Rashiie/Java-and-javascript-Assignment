6dde698c99bb646f1dba4d84609d22a67c6d5353
Rashika <104192441+Rashiie@users.noreply.github.com>
2023-10-12 20:47:40 +0530

    Update ArrayDescending.js

--- ArrayDescending.js
+++ ArrayDescending.js
@@ -1,1 +1,14 @@
+function sortArrayDescending(arr) {
+  return arr.sort((a, b) => b - a);
+}
+
+// Prompt the user for input
+const input = prompt("Enter a list of numbers separated by commas (e.g., 3, 1, 2):");
+if (input !== null) {
+  const inputArray = input.split(',').map(Number); // Split the input string into an array of numbers
+  const sortedArray = sortArrayDescending(inputArray);
+  console.log("Sorted Array (Descending): " + sortedArray);
+} else {
+  console.log("No input provided.");
+}
 
